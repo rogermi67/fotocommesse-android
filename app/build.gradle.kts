ad5cccddeacbb1a20a10e49dcc6cd41b8db42471
rogermi67 <roberto.germiniani@gmail.com>
2026-05-26 07:49:17 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "it.apconsulting.fotocommesse"
         minSdk = 29
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.4.1"
     }
 
     buildTypes {
