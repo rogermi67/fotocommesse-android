51bad20d2479a195f8cfacd5ad2d2b2a5dec99eb
rogermi67 <roberto.germiniani@gmail.com>
2026-06-18 14:20:54 +0200

    Update LandingActivity.kt

--- app/src/main/java/it/apconsulting/fotocommesse/LandingActivity.kt
+++ app/src/main/java/it/apconsulting/fotocommesse/LandingActivity.kt
@@ -3,7 +3,11 @@ package it.apconsulting.fotocommesse
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import it.apconsulting.fotocommesse.databinding.ActivityLandingBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LandingActivity : AppCompatActivity() {
 
@@ -30,6 +34,24 @@ class LandingActivity : AppCompatActivity() {
                     putExtra(Mode.EXTRA_MODE, Mode.LASTRE.name)
                 }
             )
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        refreshPhotoStats()
+    }
+
+    private fun refreshPhotoStats() {
+        lifecycleScope.launch {
+            val total = withContext(Dispatchers.IO) {
+                PhotoStorage.totalPhotoCount(this@LandingActivity)
+            }
+            val provider = SettingsManager.getSyncProviderType(this@LandingActivity)
+            binding.tvPhotoStats.text = when (provider) {
+                SyncProviderType.LOCAL_ONLY ->
+                    getString(R.string.landing_photo_stats_local, total)
+            }
         }
     }
 }
